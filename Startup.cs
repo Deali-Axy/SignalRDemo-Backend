04de23d859a08087690590a4a6ad23c53c8582f9
DealiAxy <dealiaxy@gmail.com>
2024-07-29 11:38:40 +0800

    🍆修改了net版本；启动的端口修改

--- Startup.cs
+++ Startup.cs
@@ -12,14 +12,11 @@ using Microsoft.Extensions.Hosting;
 using SignalRDemo.Hubs;
 using SignalRDemo.Services;
 
-namespace SignalRDemo
-{
-    public class Startup
-    {
+namespace SignalRDemo {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
             services.AddSignalR();
 
@@ -28,19 +25,17 @@ namespace SignalRDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
-
+            
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
                 endpoints.MapHub<CountHub>("/count");
                 endpoints.MapHub<ChatHub>("/chat");
